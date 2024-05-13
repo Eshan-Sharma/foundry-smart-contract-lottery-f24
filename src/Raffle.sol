52268eb43bd7228f06a8f840c58ebe46b3bf5af1
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-13 19:05:13 +0530

    custom error for saving gas

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -8,13 +8,19 @@ pragma solidity ^0.8.18;
  * @dev This contract implements Chainlink VRF and Chainlink Automation
  */
 contract Raffle {
+    error Raffle__NotEnoughETHSent();
+
     uint256 private immutable i_entranceFee;
 
     constructor(uint256 _entranceFee) {
         i_entranceFee = _entranceFee;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() external payable {
+        if (msg.value <= i_entranceFee) {
+            revert Raffle__NotEnoughETHSent();
+        }
+    }
 
     function pickWinner() public {}
 
