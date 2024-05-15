aa1c6d7f41a5dff75050c159c984b333aa1ee081
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-15 06:39:21 +0530

    anvil config with mocks added

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -1,7 +1,8 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.18;
 
-import {Script} from "lib/forge-std//src/Script.sol";
+import {Script} from "lib/forge-std/src/Script.sol";
+import {VRFCoordinatorV2Mock} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
 
 contract HelperConfig is Script {
     struct NetworkConfig {
@@ -23,7 +24,7 @@ contract HelperConfig is Script {
         }
     }
 
-    function getSepoliaEthConfig() public view returns (NetworkConfig memory) {
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
         return NetworkConfig({
             entranceFee: 0.01 ether,
             interval: 30,
@@ -34,16 +35,21 @@ contract HelperConfig is Script {
         });
     }
 
-    function getOrCreateAnvilEthConfig() public view returns (NetworkConfig memory) {
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
         if (activeNetworkConfig.vrfCoordinator != address(0)) {
             return activeNetworkConfig;
         }
+        uint96 baseFeed = 0.25 ether; //0.25 Link
+        uint96 gasPriceLink = 1e9; //1gwei Link
+        vm.startBroadcast();
+        VRFCoordinatorV2Mock vrfCoordinator = new VRFCoordinatorV2Mock(baseFeed, gasPriceLink);
+        vm.stopBroadcast();
         return NetworkConfig({
             entranceFee: 0.01 ether,
             interval: 30,
-            vrfCoordinator: 0xD7f86b4b8Cae7D942340FF628F82735b7a20893a,
+            vrfCoordinator: address(vrfCoordinator),
             gasLane: 0x8077df514608a09f83e4e8d300645594e5d7234665448ba83f51a50f842bd3d9,
-            subscriptionId: 0,
+            subscriptionId: 0, //Script will add this
             callbackGasLimit: 500000
         });
     }
