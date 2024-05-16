6cf1fff4762b4f70cb39c0b43e259260298b2991
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-16 08:27:19 +0530

    add: eth mainnet

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -14,9 +14,14 @@ contract HelperConfig is Script {
         uint64 subscriptionId;
         uint32 callbackGasLimit;
         address link;
+        uint256 deployerKey;
     }
 
+    uint256 public constant DEFAULT_ANVIL_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+
     NetworkConfig public activeNetworkConfig;
+
+    event HelperConfig__CreatedMockVRFCoordinator(address vrfCoordinator);
 
     constructor() {
         if (block.chainid == 11155111) {
@@ -26,7 +31,20 @@ contract HelperConfig is Script {
         }
     }
 
-    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+    function getMainnetEthConfig() public view returns (NetworkConfig memory) {
+        return NetworkConfig({
+            entranceFee: 0.01 ether,
+            interval: 30, // 30 seconds
+            vrfCoordinator: 0x271682DEB8C4E0901D1a1550aD2e64D568E69909,
+            gasLane: 0x9fe0eebf5e446e3c998ec9bb19951541aee00bb90ea201ae456421a2ded86805,
+            subscriptionId: 0, // If left as 0, our scripts will create one!
+            callbackGasLimit: 500000, // 500,000 gas
+            link: 0x514910771AF9Ca656af840dff83E8264EcF986CA,
+            deployerKey: vm.envUint("PRIVATE_KEY")
+        });
+    }
+
+    function getSepoliaEthConfig() public view returns (NetworkConfig memory) {
         return NetworkConfig({
             entranceFee: 0.01 ether,
             interval: 30,
@@ -34,7 +52,8 @@ contract HelperConfig is Script {
             gasLane: 0x8077df514608a09f83e4e8d300645594e5d7234665448ba83f51a50f842bd3d9,
             subscriptionId: 0,
             callbackGasLimit: 500000,
-            link: 0x779877A7B0D9E8603169DdbD7836e478b4624789
+            link: 0x779877A7B0D9E8603169DdbD7836e478b4624789,
+            deployerKey: vm.envUint("PRIVATE_KEY")
         });
     }
 
@@ -55,7 +74,8 @@ contract HelperConfig is Script {
             gasLane: 0x8077df514608a09f83e4e8d300645594e5d7234665448ba83f51a50f842bd3d9,
             subscriptionId: 0, //Script will add this
             callbackGasLimit: 500000,
-            link: address(link)
+            link: address(link),
+            deployerKey: DEFAULT_ANVIL_KEY
         });
     }
 }
